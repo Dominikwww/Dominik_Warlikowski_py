e9f09d8e56bf0f005cd92e9cbc83430ce6067ea0
Dominikwww <dominik.warlikowski33@gmail.com>
2024-03-03 10:37:17 +0100

    .

---
+++ 03.03.2024/03.03.2024.py
@@ -0,0 +1,30 @@
+# a = 56
+# print(a)
+# print(type(a))
+# b = 5.5
+# print(b)
+# print(type(b))
+# zmienna_tekstowa = 'Wizuazlizacja danych'
+# print(zmienna_tekstowa)
+# print(type(zmienna_tekstowa))
+
+# a = 3
+# b = 5
+# c = a*b
+# print(c)
+# c = pow(a, b)
+# print(c)
+#
+# i = 6 ** (1/2)
+# print(i)
+# i = pow(6, 1/2)
+# print(i)
+#
+# k = 'test'
+# k2 = 'test2'
+# m = 2
+# print(k+' '+k2+'\n'+str(m))
+
+a = 5
+b = 3.33
+print('liczba a jest równa {:d} {:.2f}'.format(a, b))
